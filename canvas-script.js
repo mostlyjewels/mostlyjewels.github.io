f5df3262cfcb43f0022f4d8ceb94f123531f94fe
mostlyjewels <72616712+mostlyjewels@users.noreply.github.com>
2021-01-22 10:39:57 -0500

    Update canvas-script.js

--- canvas-script.js
+++ canvas-script.js
@@ -1,9 +1,9 @@
 window.onload = function() {
 
   var canvas = document.getElementById('flappy-monster-game');
-// Game Object
+   // Game Object
   var flappyMonster = new FlappyMonster(canvas);
-  flappyMonster.start();
+    flappyMonster.start();
 
 
 };
